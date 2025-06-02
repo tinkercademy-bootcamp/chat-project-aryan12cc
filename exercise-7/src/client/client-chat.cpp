f14d91f97ccee3778fe9d348afc907a564dfbeb5
Aryan Maskara <user@ip-172-31-21-18.ap-south-1.compute.internal>
2025-06-02 17:11:47 +0000

    Add template code for clients

--- exercise-7/src/client/client-chat.cpp
+++ exercise-7/src/client/client-chat.cpp
@@ -1,1 +1,31 @@
 // ./client/client-chat.cpp
+
+/* standard headers */
+
+/* user-defined headers */
+#include "client-chat.h"
+
+namespace chat::client {
+  // --------------- PUBLIC FUNCTIONS START HERE ---------------
+
+  /*
+  Constructor for the class Client(). 
+  Initializes member variables and member functions
+  */
+  Client::Client() {
+
+  }
+
+  /*
+  Destructor for the class Client().
+  Ensures resource cleanup of member functions and variables
+  */
+  Client::~Client() {
+
+  }
+
+  // --------------- PUBLIC FUNCTIONS END HERE ---------------
+  // --------------- PRIVATE FUNCTIONS START HERE ---------------
+
+  // --------------- PRIVATE FUNCTIONS END HERE ---------------
+}
--- exercise-7/src/client/client-chat.h
+++ exercise-7/src/client/client-chat.h
@@ -1,1 +1,39 @@
 // ./client/client-chat.h
+
+#ifndef __CLIENT_CHAT_H__
+#define __CLIENT_CHAT_H__
+
+/* namespace for all client related operations */
+namespace chat::client { 
+
+  /* 
+  Client() class containing all client related operations:
+  - Username
+  - List of channels currently present in
+  (Others may be added later)
+  */
+
+  class Client {
+    
+    // public functions and variables, accessible by everyone
+    public:
+
+      /* Constructor to initialize member variables */
+      Client();
+
+      /* Destructor to ensure cleanup of variables */
+      ~Client();
+    
+    // private functions and variables, only accessible by functions and 
+    // variables of the same class
+    private:
+
+      // member functions
+
+      // member variables
+
+  };
+  
+}
+
+#endif
