579cfe4c964444584412d2010900cc7754ab2584
Aryan Maskara <user@ip-172-31-21-18.ap-south-1.compute.internal>
2025-06-03 04:11:49 +0000

    Rewrite better comments in client-main.cpp

--- exercise-7/src/client-main.cpp
+++ exercise-7/src/client-main.cpp
@@ -1,14 +1,19 @@
-// ./client-main.cpp
+// ./src/client-main.cpp
 
 /* standard headers */
-#include <iostream>
 
 /* user defined headers */
 #include "client/client-chat.h"
 
+// The port through which the client will connect to the server
+// This is the same port used by the server to listen to
+// connection requests
 const int PORT = 8080;
 
 int main() {
-  // calling the client to listen through port = PORT
-  chat::client::Client chat_client(PORT); // src/client/client-chat.h
+  // Create a client object called chat_client that calls the
+  // parameterized constructor with port = PORT, signifying to
+  // connect to the server on that specific port
+  chat::client::Client chat_client(PORT); 
+    // ./src/client/client-chat.h
 }
