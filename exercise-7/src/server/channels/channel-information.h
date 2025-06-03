4cd548166f3a7b4acf22ae73291bc178dc2641e5
Aryan Maskara <user@ip-172-31-21-18.ap-south-1.compute.internal>
2025-06-03 21:29:37 +0000

    Add member variables to channels

--- exercise-7/src/server/channels/channel-information.h
+++ exercise-7/src/server/channels/channel-information.h
@@ -4,9 +4,11 @@
 #define __CHANNEL_INFORMATION_H__
 
 /* standard headers */
+#include <string> // std::string
+#include <vector> // std::vector
 
 /* user-defined headers */
-
+#include "../../client/client-chat.h"
 
 namespace chat::server {
 
@@ -21,12 +23,30 @@ namespace chat::server {
   */
   class Channel {
 
-    // public data members and variables
+    // public functions and variables, accessible by everyone
     public:
-    
-    // priate data members and variables
+
+      // member functions
+
+      // member variables
+
+      int channel_id; // stores the id of the channel
+        // The id is assigned when the and is unique
+
+    // private functions and variables, only accessible by functions and 
+    // variables of the same class
     private:
-    
+
+      // member functions
+
+      // member variables
+
+      std::string channel_name_; // stores the name of
+        // the channel as given by the user on creation
+      std::vector<client::Client> channel_members_; // list of all clients
+        // that are the members of the channel
+      std::vector<client::Client> channel_admins_; // list of all clients
+        // that are the admins of the channel
   };
       
 } // chat::server
