b0c570b5ffb5abf83f1901c7407adf63a023436c
Aryan Maskara <user@ip-172-31-21-18.ap-south-1.compute.internal>
2025-06-03 04:16:34 +0000

    Rename PORT in client-main.cpp to CONNECTION_PORT

--- exercise-7/src/client-main.cpp
+++ exercise-7/src/client-main.cpp
@@ -8,12 +8,12 @@
 // The port through which the client will connect to the server
 // This is the same port used by the server to listen to
 // connection requests
-const int PORT = 8080;
+const int CONNECTION_PORT = 8080;
 
 int main() {
   // Create a client object called chat_client that calls the
   // parameterized constructor with port = PORT, signifying to
   // connect to the server on that specific port
-  chat::client::Client chat_client(PORT); 
+  chat::client::Client chat_client(CONNECTION_PORT); 
     // src/client/client-chat.h
 }
