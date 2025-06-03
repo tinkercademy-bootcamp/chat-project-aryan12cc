90ef85537af95cbcfe713b74522e0d30d786f3fb
Aryan Maskara <user@ip-172-31-21-18.ap-south-1.compute.internal>
2025-06-03 04:18:06 +0000

    Add verbose comments to server-main.cpp

--- exercise-7/src/server-main.cpp
+++ exercise-7/src/server-main.cpp
@@ -1,15 +1,17 @@
 // src/server-main.cpp
 
 /* standard headers */
-#include <iostream>
 
 /* user defined headers */
 #include "server/server-chat.h"
 
-const int PORT = 8080;
+// The port through which the server will listen to
+// connection requests from the client
+const int LISTENING_PORT = 8080;
 
 int main() {
-  // calling the server to listen through port = PORT
-  chat::server::Server chat_server(PORT); 
+  // calling the server to listen through a specified port
+  // by creating a server object named chat_server
+  chat::server::Server chat_server(LISTENING_PORT); 
     // src/server/server-chat.h
 }
